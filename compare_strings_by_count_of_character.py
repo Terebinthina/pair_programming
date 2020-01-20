2fc2a75ec07d4b740383b20d82a7f3515981842b
Elliot Stjernqvist <elliot.stjernqvist@gmail.com>
2020-01-20 09:58:10 +0100

    Compare strings

---
+++ compare_strings_by_count_of_character.py
@@ -0,0 +1,12 @@
+def comp(str1, str2):
+    if len(str1) == len(str2):
+        return True
+    else:
+        return False
+
+
+
+
+print(comp("AB", "CD"))
+print(comp("ABC", "DE"))
+print(comp("hello", "edabit"))
